95bb6134fc6f17a1f903a31508afce58a7e216af
Ben Allfree <ben@benallfree.com>
2015-12-05 16:08:53 -0800

    Bower setup

---
+++ resources/views/welcome.haml.blade.php
@@ -0,0 +1,12 @@
+!!!
+%html{:lang => "en"}
+  %head
+    %meta{:charset => "utf-8"}
+      %meta{:content => "IE=edge", "http-equiv" => "X-UA-Compatible"}
+        %meta{:content => "width=device-width, initial-scale=1", :name => "viewport"}
+          %title Bootstrap 101 Template
+          %link{:href => "css/bootstrap.min.css", :rel => "stylesheet"}
+  %body
+    %h1 Hello, world!
+    %script{:src => "js/all.js"}
+    %script{:src => "js/bootstrap.min.js"}
